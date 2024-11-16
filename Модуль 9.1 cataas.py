ec97044f2a6406d1375b3ca167269679590a618a
test1 <dzhuliayakuheva@gmail.com>
2024-11-16 14:07:30 +0300

    добавлено изменение размера изображения

--- Модуль 9.1 cataas.py
+++ Модуль 9.1 cataas.py
@@ -13,6 +13,7 @@ def load_image(url):
         response.raise_for_status()
         image_data=BytesIO(response.content)
         img =Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)# устанавливаем размер картинок, которые будут появляться
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"произошла ошибка: {e}")
@@ -30,7 +31,7 @@ def set_image():
 
 window=Tk()
 window.title("Cats!")
-window.geometry("600x480")
+window.geometry("600x520")
 
 
 label=Label()
