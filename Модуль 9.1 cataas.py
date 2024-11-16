0e021995c0beb39f29fa31aadafed6df97a58de6
test1 <dzhuliayakuheva@gmail.com>
2024-11-16 12:42:17 +0300

    создано окно и виджеты

--- Модуль 9.1 cataas.py
+++ Модуль 9.1 cataas.py
@@ -1,1 +1,30 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import Bytes
+
+from pygame.examples.aliens import load_image
+
+IOError
+
+
+window=Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+
+label=Label()
+label.pack()
+
+url="https://github.com/dzhuliayak/cats"
+img=load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image=img
+
+window.mainloop()
+
+
+
+
